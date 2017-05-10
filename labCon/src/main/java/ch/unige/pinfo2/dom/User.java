bd7c6a5a779eb95b6d979d2cc27d8a141d03cee5
ElCabrino <cabrini.vincent@gmail.com>
2017-05-10 18:39:32 +0200

    Correction de la stratégie de génération de la clé primaire

--- labCon/src/main/java/ch/unige/pinfo2/dom/User.java
+++ labCon/src/main/java/ch/unige/pinfo2/dom/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
 
 	@Id
 	@Column(name="ID")
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(name="FIRST_NAME")
