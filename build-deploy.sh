4d04ba2e7f07ad9e37abe55c09807d8f3cdbdd2b
Nicola Papale <nicola.papale@protonmail.ch>
2017-05-13 19:38:12 +0200

    Ajoute database de test à mariadb
    Pour que le changement prenne effet, utilisez
    `./build-deploy.sh clean db`. La base de données
    sera ensuite initialisée avec la table définie dans
    mariadb-pinfo2/user.sql.

--- build-deploy.sh
+++ build-deploy.sh
@@ -2,6 +2,19 @@
 
 # Automatic build and deploy of everything :O
 cd $(dirname $0)
+if [[ $1 == 'clean' ]] ; then
+    if [[ $2 == 'db' ]] ; then
+        echo sudo rm -rf docker-deploy/database
+        sudo rm -rf docker-deploy/database
+    elif [[ $2 == 'wildfly' ]] ; then
+        echo sudo rm -rf docker-deploy/deployments
+        sudo rm -rf docker-deploy/deployments
+    else
+        echo sudo rm -rf docker-deploy
+        sudo rm -rf docker-deploy
+    fi
+    exit
+fi
 source ./launch-servers.sh
 echo "==== Deploying... ===="
 source ./hot-deploy.sh back
