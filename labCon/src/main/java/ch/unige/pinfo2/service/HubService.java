2e3c1e209530017ff0ca42dbcdba040a1b886998
ElCabrino <cabrini.vincent@gmail.com>
2017-05-13 14:15:24 +0200

    Ajout des méthodes de l'interface pour le Hub

--- labCon/src/main/java/ch/unige/pinfo2/service/HubService.java
+++ labCon/src/main/java/ch/unige/pinfo2/service/HubService.java
@@ -3,7 +3,49 @@ package ch.unige.pinfo2.service;
 import java.util.List;
 
 public interface HubService {
-
+	
+	/**
+	 * get the consumption of a hub over a period
+	 * 
+	 * @param from
+	 * @param to
+	 * @param device id
+	 * 
+	 * @return a list of the measures over this specified period
+	 */
+	List<Long> getConsumption(String from, String to, String deviceId);
+	
+	/**
+	 * get the state (on/off) of a hub over a period
+	 * 
+	 * @param from
+	 * @param to
+	 * @param device id
+	 * 
+	 * @return a list of state of the socket over a period
+	 */
+	List<Integer> getStatus(String from, String to, String deviceId);
+	
+	/**
+	 * get the state of the sockets of the hub
+	 * 
+	 * @param from
+	 * @param to
+	 * @param device id
+	 * 
+	 * @return the current state of the hub
+	 */
+	List<Long> getStateCurrentSensor(String from, String to, String deviceId);
+	
+	/**
+	 * change the state of the hub (usefull for the admin)
+	 * 
+	 * @param ON or OFF
+	 * @param device id
+	 * 
+	 * @return true if the process succeed
+	 */
+	boolean setState(String state, String deviceIf);
 	
 
 }
--- labCon/src/main/java/ch/unige/pinfo2/service/HubServiceImpl.java
+++ labCon/src/main/java/ch/unige/pinfo2/service/HubServiceImpl.java
@@ -1,5 +1,31 @@
 package ch.unige.pinfo2.service;
 
+import java.util.List;
+
 public class HubServiceImpl implements HubService {
+
+	@Override
+	public List<Long> getConsumption(String from, String to, String deviceId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Integer> getStatus(String from, String to, String deviceId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Long> getStateCurrentSensor(String from, String to, String deviceId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean setState(String state, String deviceIf) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
