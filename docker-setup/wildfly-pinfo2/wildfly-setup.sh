18ab9370875958769f5a840471e06d9d71f3c47c
Nicola Papale <nicola.papale@protonmail.ch>
2017-05-14 16:26:14 +0200

    Enlève la configuration CORS wildfly
    Maintenant que le backend et le frontend utilisent
    un proxy sur une URL commune, il n'y a plus besoin
    d'activer les Cross Origin Requests.

--- docker-setup/wildfly-pinfo2/wildfly-setup.sh
+++ docker-setup/wildfly-pinfo2/wildfly-setup.sh
@@ -5,14 +5,6 @@ set -o nounset
 
 cd $(dirname $0)
 
-
-# Adds to wildfly the ability to answer to CORS requests, needed for
-# using extraneous libraries in the webapp. More info on CORS:
-# https://en.wikipedia.org/wiki/Cross-origin_resource_sharing 5 May 2017
-sed --in-place \
-    --file=enable-CORS.sed \
-      standalone/configuration/standalone.xml
-
 # Start up the wildfly server.
 tempdir=$(mktemp /tmp/wildfly-log-XXXXX.log)
 ./bin/standalone.sh -b 0.0.0.0 -c standalone.xml > $tempdir &
