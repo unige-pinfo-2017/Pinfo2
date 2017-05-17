73066b20fb7ec2425bc7bf0e8a2f32c680f38c98
Nicola Papale <nicola.papale@protonmail.ch>
2017-05-17 14:13:49 +0200

    Prépare la configuration pour utiliser https
    Ajoute quelque lignes dans la config nginx pour
    gérer le trafic https. Ajuste docker-compose pour
    le lancement sur terrain. Rajoute une cible "proxy"
    au script hot-deploy.sh.

--- hot-deploy.sh
+++ hot-deploy.sh
@@ -45,6 +45,14 @@ function backdeploy {
 	cd ..
 }
 
+function proxydeploy {
+	echo ==== Deploiement des clées ssl ====
+	echo cp /etc/letsencrypt/live/pinfo2.unige.ch/fullchain.pem /opt/production/seckeys/
+	sudo cp /etc/letsencrypt/live/pinfo2.unige.ch/fullchain.pem /opt/production/seckeys/
+	echo cp /etc/letsencrypt/live/pinfo2.unige.ch/privkey.pem /opt/production/seckeys/
+	sudo cp /etc/letsencrypt/live/pinfo2.unige.ch/privkey.pem /opt/production/seckeys/
+}
+
 oldoldwd="$PWD"
 cd $(dirname $0)
 if (( $# >= 1 )) ; then
@@ -52,10 +60,13 @@ if (( $# >= 1 )) ; then
 		frontdeploy ${2:-}
 	elif [ $1 = back ] ; then
 		backdeploy
+	elif [ $1 = proxy ] ; then
+		proxydeploy
 	else
 		echo "Les arguments sont:"
 		echo "  front: deploie le front end"
 		echo "  back: deploie la backend"
+		echo "  proxy: deploie les clées et certificats ssl"
 		echo "  Aucun argument deploie les deux"
 		echo "  Ce programe n'accepte qu'un argument à la fois"
 	fi
