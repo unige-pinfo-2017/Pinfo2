07aa9317850d0f625cc28fdf791b64d35f274a8c
ElCabrino <cabrini.vincent@gmail.com>
2017-05-18 13:30:01 +0200

    Correction des adresses du service Rest

--- labCon/src/main/java/ch/unige/pinfo2/service/HubServiceRs.java
+++ labCon/src/main/java/ch/unige/pinfo2/service/HubServiceRs.java
@@ -10,7 +10,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-@Path("/rest/hub")
+@Path("/devices/hubs")
 public class HubServiceRs {
 	
 	@Inject
--- labCon/src/main/java/ch/unige/pinfo2/service/LightServiceRs.java
+++ labCon/src/main/java/ch/unige/pinfo2/service/LightServiceRs.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-@Path("/restapi/devices/lights")
+@Path("/devices/lights")
 public class LightServiceRs {
 	
 	@Inject
