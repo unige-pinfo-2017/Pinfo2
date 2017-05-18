e1d25421ff69c2a5ab704a5889ad0c027b93a8fd
benibroohm <benibroohm@gmail.com>
2017-05-18 13:45:56 +0200

    Changement écran hub.

--- frontend/src/app/devices/hub.component.ts
+++ frontend/src/app/devices/hub.component.ts
@@ -34,4 +34,24 @@ export class HubComponent {
         params => this.hubId = params['id']
     );
   }
+
+  show_live() {
+        let x = document.getElementById("panel-live");
+        let y = document.getElementById("panel-history");
+
+        if (x.className.indexOf("w3-hide") !== -1 && y.className.indexOf("w3-hide") === -1) {
+            x.className = x.className.replace(" w3-hide", "");
+            y.className += " w3-hide";
+        }
+    }
+
+    show_history() {
+        let x = document.getElementById("panel-live");
+        let y = document.getElementById("panel-history");
+
+        if (y.className.indexOf("w3-hide") !== -1 && x.className.indexOf("w3-hide") === -1) {
+            y.className = y.className.replace(" w3-hide", "");
+            x.className += " w3-hide";
+        }
+    }
 }
