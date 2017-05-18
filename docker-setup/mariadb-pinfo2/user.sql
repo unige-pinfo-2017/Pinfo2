58ed28fa617dd386bda2652b24ebb2305082bec3
lebritish <tcmartin@bluewin.ch>
2017-05-18 14:18:31 +0200

    actualise les scripts de deploiement

--- docker-setup/mariadb-pinfo2/user.sql
+++ docker-setup/mariadb-pinfo2/user.sql
@@ -1,20 +1,22 @@
 #!/usr/bin/env mysql --password=admin
-CREATE DATABASE IF NOT EXISTS meskyne;
-USE meskyne;
+CREATE DATABASE IF NOT EXISTS projetinfodb;
+USE projetinfodb;
 CREATE TABLE IF NOT EXISTS RegularUser
 (
-	username VARCHAR(25) PRIMARY KEY NOT NULL,
-	firstName VARCHAR(25) NOT NULL,
-	lastName VARCHAR(25) NOT NULL,
-	password VARCHAR(25) NOT NULL,
-	token INT,
+	ID INT PRIMARY KEY NOT NULL,
+	FIRSTNAME VARCHAR(25) NOT NULL,
+	LASTNAME VARCHAR(25) NOT NULL,
+	USERNAME VARCHAR(25) NOT NULL,
+	PASSWORD VARCHAR(25) NOT NULL,
+	TOKEN INT NOT NULL,
+	ROLE ENUM('USER','MANAGER','SYSTEMADMIN') NOT NULL	
 );
 
 
 INSERT IGNORE INTO RegularUser VALUES 
-('Thomas','Martin','ThomasMartin','PasswordTM'),
-('Vincent','Cabrini','VincentCabrini','PasswordVC'),
-('Nicola','Papale','NicolaPapale','PasswordNP')
+(1,'Thomas','Martin','ThomasMartin','PasswordTM',1,'MANAGER'),
+(2,'Vincent','Cabrini','VincentCabrini','PasswordVC',1,'USER'),
+(3,'Nicola','Papale','NicolaPapale','PasswordNP',1,'SYSTEMADMIN')
 ;
 
 SELECT * FROM RegularUser
