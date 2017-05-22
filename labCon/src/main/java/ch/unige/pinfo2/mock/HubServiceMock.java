600ce2c0908a23c8617c315a3be02e6cd9db3fbf
ElCabrino <cabrini.vincent@gmail.com>
2017-05-22 12:02:40 +0200

    Finalisation du mock pour Hub et ajout d'un service qui donne l'état (on/off) des 6 sockets

--- labCon/src/main/java/ch/unige/pinfo2/mock/HubServiceMock.java
+++ labCon/src/main/java/ch/unige/pinfo2/mock/HubServiceMock.java
@@ -1,7 +1,7 @@
 package ch.unige.pinfo2.mock;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 
@@ -35,12 +35,35 @@ public class HubServiceMock implements HubService{
 				power = randomDouble(0,5);
 				current = randomDouble(0,10);
 				isOn = (long) ((r.nextBoolean())?1:0);
+				if (isOn==0){
+					power = 0d;
+					current = 0d;
+				}
 				sockets.add(new Socket(timestamp, current, power, isOn));
 			}
 			hubStates.add(new Hub(sockets));
 			timestamp+=20;		
 		}
 		return hubStates;
+	}
+
+	@Override
+	public Hub getLastState(String id) {
+		Random r = new Random();
+		List<Socket> sockets = new ArrayList<Socket>();
+		Double power;
+		Double current;
+		Long isOn;
+		Long timestamp = new Timestamp(System.currentTimeMillis()).getTime();
+		
+		for (int i=0; i<6; i++){
+			power = randomDouble(0,5);
+			current = randomDouble(0,10);
+			isOn = (long) ((r.nextBoolean())?1:0);
+			sockets.add(new Socket(timestamp, current, power, isOn));
+		}
+		
+		return (new Hub(sockets));
 	}
 
 
