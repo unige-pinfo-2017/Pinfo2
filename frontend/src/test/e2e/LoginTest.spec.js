8c3461cdf2f21368ad239f7c3102c7c468433cd3
Roderic19 <rbouesse@gmail.com>
2017-05-28 22:28:08 +0200

    Modifie et ajoute tests front end

--- frontend/src/test/e2e/LoginTest.spec.js
+++ frontend/src/test/e2e/LoginTest.spec.js
@@ -1,50 +1,241 @@
 describe("Login Tests",function(){
 
-	it("it should confirm if Login word exists", function(){
-		browser.get('http://localhost/labCon/');
+	xit("Url Login", function(){
+		browser.get('http://localhost/');
 		browser.driver.sleep(5000).then(function(){
-		var countNameLogin=element.all(by.name("Login")).count();
-		expect(countNameLogin,1);
+			expect(browser.getCurrentUrl()).toEqual('http://localhost/login?returnUrl=%2F');
 		});		
 	});
 
-	xit("it should confirm if Username word exists", function(){
-		var countNameUsername=element.all(by.name("Username")).count();
-		expect(countNameUsername,1);
+	xit("Compte le nombre de champs", function(){
+		expect(element.all(by.id("input")).count(),2);	
 	});
 
-	xit("it should confirm if Password word exists", function(){
-		var countNamePassword=element.all(by.name("Password")).count();
-		expect(countNamePassword,1);
+	xit("Compte le nombre de boutons", function(){
+		expect(element.all(by.id("button")).count(),2);	
 	});
 
-	xit("it should confirm if Login button exists", function(){
-		var countButtonLogin=element.all(by.buttonText("Login")).count();
-		expect(countButtonLogin,1);
+	xit("Verifie si il y a un bouton Login", function(){
+		expect(element.all(by.buttonText("Login")).count(),1);	
 	});
 
-	xit("it should confirm if Register button exists", function(){
-		var countButtonRegister=element.all(by.linkText("Register")).count();
-		expect(countButtonRegister,1);
+	xit("Verifie si il y a un bouton Register", function(){
+		expect(element.all(by.linkText("Register")).count(),1);
 	});
 
-	xit("it should confirm if there are 2 inputs", function(){
-		var countButtonRegister=element.all(by.tagName("input")).count();
-		expect(countButtonRegister,2);
+	xit("Ajoute un String au champ Username", function(){
+		var inputUsername=element(by.css('[name="username"]'))
+		inputUsername.sendKeys("Username");
+		browser.driver.sleep(1000).then(function(){
+			inputUsername.getAttribute("ng-reflect-model").then(function(value){
+			expect(value).toEqual("Username");
+			});
+		});
+	});
+
+	xit("Ajoute un String au champ Password", function(){
+		var inputPassword=element(by.css('[name="password"]'))
+		inputPassword.sendKeys("Password");
+		browser.driver.sleep(1000).then(function(){
+			inputPassword.getAttribute("ng-reflect-model").then(function(value){
+				expect(value).toEqual("Password");
+			});
+		});
+	});
+
+	xit("Essai Login sans password", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			var inputUsername=element(by.css('[name="username"]'))
+			inputUsername.sendKeys("Username");
+			browser.driver.sleep(1000).then(function(){
+				element.all(by.buttonText("Login")).click();
+				browser.driver.sleep(1000).then(function(){
+					var countErrorMessage=element.all(by.className("help-block")).getText();
+		
+					expect(countErrorMessage).toEqual(['Password is required']);
+				});
+			});
+		});
+	});
+
+	xit("Essai Login sans Username", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("Password");
+			browser.driver.sleep(1000).then(function(){
+				element.all(by.buttonText("Login")).click();
+				browser.driver.sleep(1000).then(function(){
+					var countErrorMessage=element.all(by.className("help-block")).getText();
+		
+					expect(countErrorMessage).toEqual(['Username is required']);
+				});
+			});
+		});
+	});
+
+	xit("Essai Login sans Username et sans Password", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			element.all(by.buttonText("Login")).click();
+			browser.driver.sleep(1000).then(function(){
+				var countErrorMessage=element.all(by.className("help-block")).count();
+				expect(countErrorMessage,2);
+				expect(element.all(by.className("help-block")).getText()).toEqual(['Username is required', 'Password is required']);
+			});		
+		});
+	});
+	//PEUT ETRE UN BUG
+	xit("Essai acces menu hub sans login", function(){
+		browser.get('http://localhost/hub');
+		browser.driver.sleep(5000).then(function(){
+			expect(browser.getCurrentUrl()).toEqual('http://localhost/login?returnUrl=%2Fhub');
+		});
+	});
+
+	xit("Essai acces menu light sans login", function(){
+		browser.get('http://localhost/light');
+		browser.driver.sleep(5000).then(function(){
+			expect(browser.getCurrentUrl()).toEqual('http://localhost/login?returnUrl=%2F');
+		});
 	});
 	
-	it("it should add value to input username", function(){
-		element(by.css('[name="username"]')).sendKeys("aaaaaaaaa");
-		browser.driver.sleep(1000).then(function(){
-			expect(element(by.css('[name="username"]')).getKeys()).toEqual("aaaaaaaaa");
+	xit("Essai acces menu socket sans login", function(){
+		browser.get('http://localhost/socket');
+		browser.driver.sleep(5000).then(function(){
+			expect(browser.getCurrentUrl()).toEqual('http://localhost/login?returnUrl=%2F');
 		});
 	});
 
-	xit("it should add value to input password", function(){
-		element(by.css('[name="password"]')).sendKeys("Password");
-		browser.driver.sleep(1000).then(function(){
-			expect(element(by.css('[name="password"]'))).toEqual("Password");
+	xit("Essai acces hub particulier sans login", function(){
+		browser.get('http://localhost/hub/110');
+		browser.driver.sleep(5000).then(function(){
+			expect(browser.getCurrentUrl()).toEqual('http://localhost/login?returnUrl=%2Fhub%2F110');
 		});
 	});
 
+	xit("Essai acces light particulier sans login", function(){
+		browser.get('http://localhost/light/100');
+		browser.driver.sleep(5000).then(function(){
+			expect(browser.getCurrentUrl()).toEqual('http://localhost/login?returnUrl=%2Flight%2F100');
+		});
+	});
+
+	xit("Essai acces socket particulier sans login", function(){
+		browser.get('http://localhost/socket/200');
+		browser.driver.sleep(5000).then(function(){
+			expect(browser.getCurrentUrl()).toEqual('http://localhost/login?returnUrl=%2Fsocket%2F200');
+		});
+	});
+
+
+	xit("Bon Username mauvais password", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			var inputUsername=element(by.css('[name="username"]'))
+			inputUsername.sendKeys("oui");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("jkhf");
+			browser.driver.sleep(1000).then(function(){
+				var countErrorMessage=element.all(by.name("Response with status: 401 Unauthorized for URL: http://localhost/restapi/login")).count();
+        			expect(countErrorMessage,1);
+			});
+		});
+	});
+
+	xit("Bon Password mauvais username", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			var inputUsername=element(by.css('[name="username"]'))
+			inputUsername.sendKeys("jkhf");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("oui");
+			browser.driver.sleep(1000).then(function(){
+				var countErrorMessage=element.all(by.name("Response with status: 401 Unauthorized for URL: http://localhost/restapi/login")).count();
+        			expect(countErrorMessage,1);
+			});
+		});
+	});
+
+	xit("Mauvais Password mauvais username", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			var inputUsername=element(by.css('[name="username"]'))
+			inputUsername.sendKeys("jkhf");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("jkhf");
+			browser.driver.sleep(1000).then(function(){
+				var countErrorMessage=element.all(by.name("Response with status: 401 Unauthorized for URL: http://localhost/restapi/login")).count();
+        			expect(countErrorMessage,1);
+			});
+		});
+	});
+
+	xit("Login->Register", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			element.all(by.linkText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				browser.driver.sleep(5000).then(function(){
+					expect(browser.getCurrentUrl()).toEqual('http://localhost/register');
+				});
+			});
+		});
+	});
+
+	xit("Login->Register->Login", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			element.all(by.linkText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				element.all(by.linkText("Cancel")).click();
+				browser.driver.sleep(1000).then(function(){
+				expect(browser.getCurrentUrl()).toEqual('http://localhost/login');
+
+				});
+			});
+		});
+	});
+
+	xit("Login", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			var inputUsername=element(by.css('[name="username"]'))
+			inputUsername.sendKeys("oui");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("oui");
+			browser.driver.sleep(1000).then(function(){
+				element.all(by.buttonText("Login")).click();
+				browser.driver.sleep(5000).then(function(){
+					expect(browser.getCurrentUrl()).toEqual('http://localhost/login?returnUrl=%2F');
+					expect(element.all(by.name("You're logged in oui !")).count(),1);
+				});
+			});
+		});
+	});
+
+	xit("Mot Username existe", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			var countNameUsername=element.all(by.name("Username")).count();
+			expect(countNameUsername,1);
+		});
+	});
+
+	xit("Mot Password existe", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			var countNamePassword=element.all(by.name("Password")).count();
+			expect(countNamePassword,1);
+		});
+	});
+
+	xit("Mot Login existe", function(){
+		browser.get('http://localhost/');
+		browser.driver.sleep(5000).then(function(){
+			var countNameLogin=element.all(by.name("Login")).count();
+			expect(countNameLogin,1);
+		});		
+	});
 });
--- frontend/src/test/e2e/RegisterTest.spec.js
+++ frontend/src/test/e2e/RegisterTest.spec.js
@@ -1,46 +1,329 @@
-describe("Login Tests",function(){
+describe("Register Tests",function(){
 
-	xit("it should confirm if Register word exists", function(){
+	xit("Url Register", function(){
 		browser.get('http://localhost/register');
 		browser.driver.sleep(5000).then(function(){
-		var countNameLogin=element.all(by.name("Register")).count();
-		expect(countNameLogin,1);
+			expect(browser.getCurrentUrl()).toEqual('http://localhost/register');
 		});		
 	});
 
-	xit("it should confirm if Firstname word exists", function(){
-		var countNameUsername=element.all(by.name("First Name")).count();
-		expect(countNameUsername,1);
+	xit("Verification 4 inputs", function(){
+		var countInput=element.all(by.id("input")).count();
+		expect(countInput,4);		
 	});
 
-	xit("it should confirm if Lastname word exists", function(){
-		var countNamePassword=element.all(by.name("Last Name")).count();
-		expect(countNamePassword,1);
+	xit("Nom First Name", function(){
+		var countFirstName=element.all(by.name("First Name")).count();
+		expect(countFirstName,1);		
 	});
 
-	xit("it should confirm if Username button exists", function(){
-		var countNameUsername=element.all(by.name("Username")).count();
-		expect(countNameUsername,1);
+	xit("Nom Last Name", function(){
+		var countLastName=element.all(by.name("Last Name")).count();
+		expect(countLastName,1);		
 	});
 
-	xit("it should confirm if Password button exists", function(){
-		var countNamePassword=element.all(by.name("Password")).count();
-		expect(countNamePassword,1);
+	xit("Nom Username", function(){
+		var countUsername=element.all(by.name("Username")).count();
+		expect(countUsername,1);
 	});
 
-	xit("it should confirm if there are 4 inputs", function(){
-		var countButtonRegister=element.all(by.tagName("input")).count();
-		expect(countButtonRegister,4);
+	xit("Nom Password", function(){
+		var countPassword=element.all(by.name("Password")).count();
+		expect(countPassword,1);		
 	});
 
-	xit("it should confirm if Register button exists", function(){
-		var countButtonRegister=element.all(by.buttonText("Register")).count();
-		expect(countButtonRegister,1);
+	xit("Compte le nombre de boutons", function(){
+		expect(element.all(by.id("button")).count(),2);	
 	});
 
-	xit("it should confirm if Cancel button exists", function(){
-		var countButtonRegister=element.all(by.linkText("Cancel")).count();
-		expect(countButtonRegister,1);
+	xit("Verifie si il y a un bouton Register", function(){
+		expect(element.all(by.buttonText("Register")).count(),1);	
+	});
+
+	xit("Verifie si il y a un bouton Cancel", function(){
+		expect(element.all(by.linkText("Cancel")).count(),1);
+	});
+	
+	xit("Essai Register sans FirstName", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputLastName=element(by.css('[name="lastName"]'))
+			inputLastName.sendKeys("LastName");
+			var inputUsername=element(by.css('[name="username"]'))
+			inputUsername.sendKeys("Username");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("Password");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['First Name is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans LastName", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputFirstName=element(by.css('[name="firstName"]'))
+			inputFirstName.sendKeys("FirstName");
+			var inputUsername=element(by.css('[name="username"]'))
+			inputUsername.sendKeys("Username");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("Password");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['Last Name is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans UserName", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputFirstName=element(by.css('[name="firstName"]'))
+			inputFirstName.sendKeys("FirstName");
+			var inputLastname=element(by.css('[name="lastName"]'))
+			inputLastname.sendKeys("lastName");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("Password");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['Username is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans Password", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputFirstName=element(by.css('[name="firstName"]'))
+			inputFirstName.sendKeys("FirstName");
+			var inputLastname=element(by.css('[name="lastName"]'))
+			inputLastname.sendKeys("lastName");
+			var inputUsername=element(by.css('[name="username"]'))
+			inputUsername.sendKeys("Username");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['Password is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans FirstName et sans LastName", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputUsername=element(by.css('[name="username"]'))
+			inputUsername.sendKeys("Username");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("Password");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['First Name is required', 'Last Name is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans FirstName et sans Username", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputLastname=element(by.css('[name="lastName"]'))
+			inputLastname.sendKeys("Lastname");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("Password");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['First Name is required', 'Username is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans FirstName et sans Password", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputLastname=element(by.css('[name="lastName"]'))
+			inputLastname.sendKeys("Lastname");
+			var inputPassword=element(by.css('[name="username"]'))
+			inputPassword.sendKeys("Username");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['First Name is required', 'Password is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans LastName et sans Username", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputLastname=element(by.css('[name="firstName"]'))
+			inputLastname.sendKeys("Firstname");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("Password");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['Last Name is required', 'Username is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans LastName et sans Password", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputLastname=element(by.css('[name="firstName"]'))
+			inputLastname.sendKeys("Firstname");
+			var inputPassword=element(by.css('[name="username"]'))
+			inputPassword.sendKeys("Username");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['Last Name is required', 'Password is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans Username et sans Password", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputLastname=element(by.css('[name="firstName"]'))
+			inputLastname.sendKeys("Firstname");
+			var inputPassword=element(by.css('[name="lastName"]'))
+			inputPassword.sendKeys("Lastname");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['Username is required', 'Password is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans LastName, sans Username et sans Password", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputLastname=element(by.css('[name="firstName"]'))
+			inputLastname.sendKeys("Firstname");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['Last Name is required', 'Username is required', 'Password is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans FirstName, sans Username et sans Password", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputLastname=element(by.css('[name="lastName"]'))
+			inputLastname.sendKeys("Lastname");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['First Name is required', 'Username is required', 'Password is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans FirstName, sans Lastname et sans Password", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputLastname=element(by.css('[name="username"]'))
+			inputLastname.sendKeys("Username");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['First Name is required', 'Last Name is required', 'Password is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans FirstName, sans Lastname et sans Username", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputLastname=element(by.css('[name="password"]'))
+			inputLastname.sendKeys("Password");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['First Name is required', 'Last Name is required', 'Username is required']);
+			});		
+		});
+	});
+
+	xit("Essai Register sans FirstName, sans Lastname, sans Username et sans Password", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				expect(element.all(by.className("help-block")).getText()).toEqual(['First Name is required', 'Last Name is required', 'Username is required', 'Password is required']);
+			});		
+		});
+	});
+
+	xit("Verification Cancel", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			element.all(by.linkText("Cancel")).click();
+			browser.driver.sleep(5000).then(function(){
+				expect(browser.getCurrentUrl()).toEqual('http://localhost/login');
+			});		
+		});
+	});
+
+	xit("Register User déjà crée", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputFirstName=element(by.css('[name="firstName"]'))
+			inputFirstName.sendKeys("oui");
+			var inputLastname=element(by.css('[name="lastName"]'))
+			inputLastname.sendKeys("oui");
+			var inputFirstName=element(by.css('[name="username"]'))
+			inputFirstName.sendKeys("oui");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("oui");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(1000).then(function(){
+				var countErrorMessage=element.all(by.name("Response with status: 401 Unauthorized for URL: http://localhost/restapi/login")).count();
+        			expect(countErrorMessage,1);
+			});		
+		});
+	});
+
+	xit("Register User non crée", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputFirstName=element(by.css('[name="firstName"]'))
+			inputFirstName.sendKeys("oui5");
+			var inputLastname=element(by.css('[name="lastName"]'))
+			inputLastname.sendKeys("oui5");
+			var inputFirstName=element(by.css('[name="username"]'))
+			inputFirstName.sendKeys("oui4");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("oui5");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(5000).then(function(){
+        			var countMessage=element.all(by.name("Registration successful")).count();
+        			expect(countMessage,1);
+			});		
+		});
+	});
+
+	it("Register User non crée et log in", function(){
+		browser.get('http://localhost/register');
+		browser.driver.sleep(5000).then(function(){
+			var inputFirstName=element(by.css('[name="firstName"]'))
+			inputFirstName.sendKeys("oui5");
+			var inputLastname=element(by.css('[name="lastName"]'))
+			inputLastname.sendKeys("oui5");
+			var inputUsername=element(by.css('[name="username"]'))
+			inputUsername.sendKeys("ouiTestLog");
+			var inputPassword=element(by.css('[name="password"]'))
+			inputPassword.sendKeys("oui5");
+			element.all(by.buttonText("Register")).click();
+			browser.driver.sleep(5000).then(function(){
+        			var inputUsernameLog=element(by.css('[name="username"]'))
+				inputUsernameLog.sendKeys("ouiTestLog");
+				var inputPasswordLog=element(by.css('[name="password"]'))
+				inputPasswordLog.sendKeys("oui5");
+				browser.driver.sleep(1000).then(function(){
+					element.all(by.buttonText("Login")).click();
+					browser.driver.sleep(5000).then(function(){
+						expect(browser.getCurrentUrl()).toEqual('http://localhost/');
+						expect(element.all(by.name("You're logged in oui5 !")).count(),1);
+				});
+			});
+			});		
+		});
 	});
 
 
