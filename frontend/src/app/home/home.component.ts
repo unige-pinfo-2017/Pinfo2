77c1465ea267364ede0e379b4789f14d5115ede1
Maximus <maxc2507@gmail.com>
2017-05-31 18:50:34 +0200

    Ajout de la table "vu Users" pour le SysAdmin
    Ajout d'une table pour que le SysAdmin puisse voir les Users Connecté (nom), sur quelle workspace, leur ID

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-﻿import { Component, OnInit } from '@angular/core';
-import { User } from '../_models/index';
+﻿import { User } from './../_models/user';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../_services/index';
 import { Light } from '../_models/light';
 import { Workstation } from '../_models/workstation';
@@ -15,10 +15,31 @@ export class HomeComponent implements OnInit{
     title = 'Home';
     currentUser: User;
     selectedWs: Workstation;
+    UTILISATEURS: Hero[] = [
+      {id: 1, workspace: 10, name:'Fred',},
+      {id: 2, workspace: 3,  name:'Max'},
+      {id: 5, workspace: 27, name:'Thomas'},
+      {id: 3, workspace: 44, name:'Nico'},
+      {id: 4, workspace: 67, name:'Vinc'},
+      {id: 6, workspace: 99, name:'Beni'},
+      {id: 7, workspace: 66, name:'Rodé'},
+    ];
+    users = this.UTILISATEURS;
     constructor(private userService: UserService) {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
     }
 
     ngOnInit() {
     }
+
+    whichRole(){
+      return localStorage.getItem('Role');
+    }
 }
+
+export class Hero  {
+  id : number;
+  name : string;
+  workspace : number;
+}
+
