96f7acac65d7d27672e0fa78435088f5fac24e67
Maximus <maxc2507@gmail.com>
2017-05-31 18:45:48 +0200

    Ajout et modification des roles sur la page Home
    - Ajout des Roles des l'authentification
    - Classe l'affichage de la page Home en fonction des Rôles

--- frontend/src/app/_services/authentication.service.ts
+++ frontend/src/app/_services/authentication.service.ts
@@ -19,6 +19,7 @@ export class AuthenticationService {
                 if (user && user.token) {
                     // store user details and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify(user));
+                    localStorage.setItem('Role', 'User');
                 }
             });
     }
