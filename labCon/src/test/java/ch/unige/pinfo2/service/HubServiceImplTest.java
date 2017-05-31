897a117c8d50b89fc770f3d9b5a7cfa5d39cfb10
Roderic19 <rbouesse@gmail.com>
2017-05-31 20:26:44 +0200

    Ajoute tests Hub et ajoute des données dans la base de données pour la table Device

--- labCon/src/test/java/ch/unige/pinfo2/service/HubServiceImplTest.java
+++ labCon/src/test/java/ch/unige/pinfo2/service/HubServiceImplTest.java
@@ -1,82 +1,227 @@
 package ch.unige.pinfo2.service;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.junit.Test;
 
 import ch.unige.pinfo2.dom.Hub;
+import ch.unige.pinfo2.dom.Light;
+import ch.unige.pinfo2.dom.Socket;
 import ch.unige.pinfo2.mock.HubServiceImplMock;
+import ch.unige.pinfo2.mock.LightServiceImplMock;
 import junit.framework.Assert;
 
 @SuppressWarnings("deprecation")
 public class HubServiceImplTest{
 	
-	HubService hs;
+	@Inject
+	private HubService hs;
+	
 	
 	@Test
-	public void testGetLastStateSizeListHubs(){
+	public void testCountSockets(){
 		hs=new HubServiceImplMock();
 		Hub hub = hs.getLastState("1");
 		Assert.assertEquals(6, hub.getSockets().size());
 	}
 	
-	
-	
-	
 	@Test
-	public void testHubGetStateNotEmpty(){
+	public void testLastStatePowerNotNull(){
 		hs=new HubServiceImplMock();
-		Assert.assertEquals(1, hs.getState("1", 2000L, 2010L).size());
+		Hub hub = hs.getLastState("1");
+		List<Socket> sockets=hub.getSockets();
+		Boolean flag=true;
+		for (Socket s: sockets){
+			if(s.getPower()==null){
+				flag=false;
+			}
+		}
+		Assert.assertTrue(flag);
 	}
 	
 	@Test
-	public void testHubGetStateEmpty(){
+	public void testLastStateCurrentNotNull(){
 		hs=new HubServiceImplMock();
-		Assert.assertEquals(0, hs.getState("1", 2010L, 2010L).size());
+		Hub hub = hs.getLastState("1");
+		List<Socket> sockets=hub.getSockets();
+		Boolean flag=true;
+		for (Socket s: sockets){
+			if(s.getCurrent()==null){
+				flag=false;
+			}
+		}
+		Assert.assertTrue(flag);
 	}
 	
 	@Test
-	public void testHubGetStateLength(){
+	public void testLastStateTimestampNotNull(){
 		hs=new HubServiceImplMock();
-		Long fromValue=1980L;
-		Long toValue=3000L;
-		Assert.assertEquals((toValue-fromValue)/20, hs.getState("1", fromValue, toValue).size());	
+		Hub hub = hs.getLastState("1");
+		List<Socket> sockets=hub.getSockets();
+		Boolean flag=true;
+		for (Socket s: sockets){
+			if(s.getTimestamp()==null){
+				flag=false;
+			}
+		}
+		Assert.assertTrue(flag);
+	}
+	
+	
+	@Test
+	public void testLastStatePowerPositive(){
+		hs=new HubServiceImplMock();
+		Hub hub = hs.getLastState("1");
+		List<Socket> sockets=hub.getSockets();
+		Boolean flag=true;
+		for (Socket s: sockets){
+			if(s.getPower()<0){
+				flag=false;
+			}
+		}
+		Assert.assertTrue(flag);
 	}
 	
 	@Test
-	public void testHubGetStateDeviceIDNull(){
+	public void testLastStateCurrentPositive(){
 		hs=new HubServiceImplMock();
-		Assert.assertEquals(0, hs.getState(null, 2020L, 2010L).size());
+		Hub hub = hs.getLastState("1");
+		List<Socket> sockets=hub.getSockets();
+		Boolean flag=true;
+		for (Socket s: sockets){
+			if(s.getCurrent()<0){
+				flag=false;
+			}
+		}
+		Assert.assertTrue(flag);
 	}
 	
 	@Test
-	public void testHubGetStateFromValueNull(){
+	public void testLastStateTimestampPositive(){
 		hs=new HubServiceImplMock();
-		Assert.assertEquals(1, hs.getState("1", null, 2010L).size());
+		Hub hub = hs.getLastState("1");
+		List<Socket> sockets=hub.getSockets();
+		Boolean flag=true;
+		for (Socket s: sockets){
+			if(s.getTimestamp()<0){
+				flag=false;
+			}
+		}
+		Assert.assertTrue(flag);
 	}
 	
 	@Test
-	public void testHubGetStateToValueNull(){
+	public void testGetStateCurrentNotNull(){
 		hs=new HubServiceImplMock();
-		Assert.assertEquals(1, hs.getState("1", 2000L, null).size());
+		List<Hub> hubs = hs.getState("1", 2000L, 3000L);
+		Boolean flag=true;
+		for (Hub h: hubs){
+			for (Socket s:h.getSockets()){
+				if(s.getCurrent()==null){
+					flag=false;
+				}
+			}
+		}
+		Assert.assertTrue(flag);
 	}
 	
 	@Test
-	public void testHubGetStateFromToValuesNull(){
+	public void testGetStateCurrentPositive(){
 		hs=new HubServiceImplMock();
-		Assert.assertEquals(1, hs.getState("1", null, null).size());
+		List<Hub> hubs = hs.getState("1", 2000L, 3000L);
+		Boolean flag=true;
+		for (Hub h: hubs){
+			for (Socket s:h.getSockets()){
+				if(s.getCurrent()<0){
+					flag=false;
+				}
+			}
+		}
+		Assert.assertTrue(flag);
 	}
 	
 	@Test
-	public void testHubGetStateSameValues(){
+	public void testGetStatePowerNotNull(){
 		hs=new HubServiceImplMock();
-		Long fromValue=2000L;
-		Long toValue=2000L;
-		Assert.assertEquals((toValue-fromValue)/20, hs.getState("1", fromValue, toValue).size());	
+		List<Hub> hubs = hs.getState("1", 2000L, 3000L);
+		Boolean flag=true;
+		for (Hub h: hubs){
+			for (Socket s:h.getSockets()){
+				if(s.getPower()==null){
+					flag=false;
+				}
+			}
+		}
+		Assert.assertTrue(flag);
 	}
 	
 	@Test
-	public void testHubGetStateTwoLists(){
+	public void testGetStatePowerPositive(){
 		hs=new HubServiceImplMock();
-		Assert.assertNotSame(hs.getState("1", 1980L, 2020L).size(), hs.getState("2", 2000L, 2010L).size());	
+		List<Hub> hubs = hs.getState("1", 2000L, 3000L);
+		Boolean flag=true;
+		for (Hub h: hubs){
+			for (Socket s:h.getSockets()){
+				if(s.getPower()<0){
+					flag=false;
+				}
+			}
+		}
+		Assert.assertTrue(flag);
+	}
+	
+	@Test
+	public void testGetStateTimestampNotNull(){
+		hs=new HubServiceImplMock();
+		List<Hub> hubs = hs.getState("1", 2000L, 3000L);
+		Boolean flag=true;
+		for (Hub h: hubs){
+			for (Socket s:h.getSockets()){
+				if(s.getTimestamp()==null){
+					flag=false;
+				}
+			}
+		}
+		Assert.assertTrue(flag);
+	}
+	
+	
+	@Test
+	public void testGetStateTimestampPositive(){
+		hs=new HubServiceImplMock();
+		List<Hub> hubs = hs.getState("1", 2000L, 3000L);
+		Boolean flag=true;
+		for (Hub h: hubs){
+			for (Socket s:h.getSockets()){
+				if(s.getTimestamp()<0){
+					flag=false;
+				}
+			}
+		}
+		Assert.assertTrue(flag);
+	}
+	
+	@Test
+	public void testGetStateNotNull(){
+		hs=new HubServiceImplMock();
+		List<Hub> hubs = hs.getState("1", 2000L, 2000L);
+		Assert.assertNotNull(hubs);
+	}
+	
+	@Test
+	public void testStatesSizeNotZero(){
+		hs=new HubServiceImplMock();
+		List<Hub> hubs = hs.getState("1", 2000L, 3000L);
+		Assert.assertTrue(hubs.size()>0);
+	}
+	
+	@Test
+	public void testStatesSizeZero2(){
+		hs=new HubServiceImplMock();
+		List<Hub> hubs = hs.getState("1", 3000L, 2000L);
+		Assert.assertEquals(hubs.size(),0);
 	}
 
 }
--- labCon/src/test/java/ch/unige/pinfo2/service/LightServiceImplTest.java
+++ labCon/src/test/java/ch/unige/pinfo2/service/LightServiceImplTest.java
@@ -1,12 +1,164 @@
 package ch.unige.pinfo2.service;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.junit.Test;
+
+import ch.unige.pinfo2.dom.Light;
 import ch.unige.pinfo2.mock.LightServiceImplMock;
 import junit.framework.Assert;
 
 @SuppressWarnings("deprecation")
 public class LightServiceImplTest {
 	
-	LightService ls;
+	@Inject
+	private LightService ls;
+	
+	@Test
+	public void testLastStatePowerNotNull(){
+		ls=new LightServiceImplMock();
+		Light light = ls.getLastState("1");
+		Assert.assertNotNull(light.getPower());
+	}
+	
+	@Test
+	public void testLastStatePowerPositive(){
+		ls=new LightServiceImplMock();
+		Light light = ls.getLastState("1");
+		Assert.assertTrue(light.getPower()>=0);
+	}
+	
+	@Test
+	public void testLastStateTimestampNotNull(){
+		ls=new LightServiceImplMock();
+		Light light = ls.getLastState("1");
+		Assert.assertNotNull(light.getTimestamp());
+	}
+	
+	//Bug ici, timestamp peut etre négatif
+	@Test
+	public void testLastStateTimestampPositive(){
+		ls=new LightServiceImplMock();
+		Light light = ls.getLastState("1");
+		Assert.assertTrue(light.getTimestamp()>=0);
+	}
+	
+	@Test
+	public void testLastStateStatusOn(){
+		ls=new LightServiceImplMock();
+		Light light = ls.getLastState("1");
+		Assert.assertNotNull(light.getOnOffStatus());
+	}
+	
+	@Test
+	public void testLastStateStatusNotNull(){
+		ls=new LightServiceImplMock();
+		Light light = ls.getLastState("1");
+		Assert.assertNotNull(light.getOnOffStatus());
+	}
+	
+	@Test
+	public void testStatesNotNull(){
+		ls=new LightServiceImplMock();
+		List<Light> lights = ls.getStates("1", 2000L, 2000L);
+		Assert.assertNotNull(lights);
+	}
+	
+	/*@Test
+	public void testStatesSizeZero(){
+		ls=new LightServiceImplMock();
+		List<Light> lights = ls.getStates("1", 2000L, 2000L);
+		Assert.assertTrue(lights.size()>=0);
+	}*/
+	
+	@Test
+	public void testStatesSizeNotZero(){
+		ls=new LightServiceImplMock();
+		List<Light> lights = ls.getStates("1", 2000L, 3000L);
+		Assert.assertTrue(lights.size()>0);
+	}
+	
+	@Test
+	public void testStatesSizeZero2(){
+		ls=new LightServiceImplMock();
+		List<Light> lights = ls.getStates("1", 3000L, 2000L);
+		Assert.assertEquals(lights.size(),0);
+	}
+	
+	@Test
+	public void testStatesPowerNotNull(){
+		ls=new LightServiceImplMock();
+		List<Light> lights = ls.getStates("1", 2000L, 3000L);
+		Boolean flag=true;
+		for (Light l: lights){
+			if(l.getPower()==null){
+				flag=false;
+			}
+		}
+		Assert.assertTrue(flag);
+	}
+	
+	@Test
+	public void testStatesPowerPositive(){
+		ls=new LightServiceImplMock();
+		List<Light> lights = ls.getStates("1", 2000L, 3000L);
+		Boolean flag=true;
+		for (Light l: lights){
+			if(l.getPower()<0){
+				flag=false;
+			}
+		}
+		Assert.assertTrue(flag);
+	}
+	
+	@Test
+	public void testStatesTimestampNotNull(){
+		ls=new LightServiceImplMock();
+		List<Light> lights = ls.getStates("1", 2000L, 3000L);
+		Boolean flag=true;
+		for (Light l: lights){
+			if(l.getTimestamp()==null){
+				flag=false;
+			}
+		}
+		Assert.assertTrue(flag);
+	}
+	
+	@Test
+	public void testStatesTimestampPositive(){
+		ls=new LightServiceImplMock();
+		List<Light> lights = ls.getStates("1", 2000L, 3000L);
+		Boolean flag=true;
+		for (Light l: lights){
+			if(l.getTimestamp()<0){
+				flag=false;
+			}
+		}
+		Assert.assertTrue(flag);
+	}
+	
+	
+	
+	@Test
+	public void testStatesGetOn(){
+		ls=new LightServiceImplMock();
+		List<Light> lights = ls.getStates("1", 2000L, 3000L);
+		Boolean flag=true;
+		for (Light l: lights){
+			if(l.getPower()==0){
+				if(l.getOnOffStatus()==true){
+					flag=false;
+				}
+			}
+			else{
+				if(l.getOnOffStatus()==false){
+					flag=false;
+				}
+			}
+		}
+		Assert.assertTrue(flag);
+	}
 	
 }
